0be0e306b2997c2e609644a264c346c12a2e2c75
Michael Powers <126989418+michaelpowers8@users.noreply.github.com>
2025-05-18 19:16:14 -0500

    Update Verbal_Memory.py

--- Verbal_Memory.py
+++ Verbal_Memory.py
@@ -7,36 +7,30 @@ from time import sleep
 from warnings import filterwarnings
 from Global import *
 
+def click_seen_new_button(driver:Chrome,button_string:str,score:int) -> int:
+    # Wait for the button to be clickable and then click it
+    try:
+        button:WebElement = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, f"//button[contains(@class, 'css-de05nr') and contains(@class, 'e19owgy710') and text()='{button_string}']"))
+        )
+        
+        button.click()
+        score += 1
+        return score
+    except Exception as e:
+        logger.critical(f"Error clicking the SEEN button. Official Error: {e}. Terminating the program.")
+        raise Exception(f"{str(e)}")
+
 def play(driver:Chrome,words:list[str],score:int,logger:Logger) -> tuple[list[str],int]|Exception:
     try:
         # Wait for the word container to load
         word:str = driver.find_element(By.CSS_SELECTOR, "div.css-1qvtbrk.e19owgy78 div.word").text
         if(word in words):
-            # Wait for the button to be clickable and then click it
-            try:
-                seen_button:WebElement = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'css-de05nr') and contains(@class, 'e19owgy710') and text()='SEEN']"))
-                )
-                
-                seen_button.click()
-                score += 1
-                
-            except Exception as e:
-                logger.critical(f"Error clicking the SEEN button. Official Error: {e}. Terminating the program.")
-                raise Exception(f"{str(e)}")
+            score = click_seen_new_button(driver,'SEEN',score)
         else:
             # Wait for the button to be clickable and then click it
             words.append(word)
-            try:
-                new_button:WebElement = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'css-de05nr') and contains(@class, 'e19owgy710') and text()='NEW']"))
-                )
-                
-                new_button.click()
-                score += 1
-            except Exception as e:
-                logger.critical(f"Error clicking the NEW button. Official Error: {e}. Terminating the program.")
-                raise Exception(f"{str(e)}")
+            score = click_seen_new_button(driver,'NEW',score)
         return words,score
     except Exception as e:
         logger.critical(f"Exception occurred while playing. Official Error: {str(e)}")
@@ -88,32 +82,28 @@ if __name__ == "__main__":
     if(not(verbal_memory)):
         logger.info("Verbal Memory in config.json set to false. Terminating program.")
     else:
-        try:
-            words:list[str] = []
-            score:int = 0
-            driver.get("https://humanbenchmark.com/login")
+        words:list[str] = []
+        score:int = 0
+        driver.get("https://humanbenchmark.com/login")
 
-            input_username(driver,username,logger)
-            input_password(driver,password,logger)
-            click_login(driver,logger)  
-                
-            sleep(3) # Wait time to ensure full page loads
-
-            open_game(driver,logger,"verbal-memory")
-            start_game(driver,logger,"verbal-memory")
+        input_username(driver,username,logger)
+        input_password(driver,password,logger)
+        click_login(driver,logger)  
             
-            while score < 9950: # Human benchmark crashes at a score beyond 10,000, so this is the maximum.
-                words,score = play(driver,words,score,logger)
-                if(score%500==0):
-                    logger.info(f"Current Verbal Memory Score: {score:,.0f}")
+        sleep(3) # Wait time to ensure full page loads
 
-            for _ in range(3):
-                words,score = lose(driver,words,score,logger)
+        open_game(driver,logger,"verbal-memory")
+        start_game(driver,logger,"verbal-memory")
+        
+        while score < 9950: # Human benchmark crashes at a score beyond 10,000, so this is the maximum.
+            words,score = play(driver,words,score,logger)
+            if(score%500==0):
+                logger.info(f"Current Verbal Memory Score: {score:,.0f}")
 
-            sleep(post_test_delay)
-            save_score(driver,logger)
+        for _ in range(3):
+            words,score = lose(driver,words,score,logger)
 
-            driver.close()
-        except Exception as e:
-            logger.critical(f"Game crashed. Official error: {str(e)}. Terminating the program.")
-            raise Exception(f"Game crashed. Official error: {str(e)}. Terminating the program.")
+        sleep(post_test_delay)
+        save_score(driver,logger)
+
+        driver.close()
