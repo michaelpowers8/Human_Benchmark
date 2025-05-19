8378ebd94626dc4f25d8a837bfb60869b3777381
Michael Powers <126989418+michaelpowers8@users.noreply.github.com>
2025-05-18 19:18:57 -0500

    Update Verbal_Memory.py

--- Verbal_Memory.py
+++ Verbal_Memory.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver import Chrome,ChromeOptions
+from selenium.webdriver import Chrome
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
